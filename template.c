eeace0ff8edb695f9d640537859b25bdf2ea775f
CUICUI2010 <3347645113@qq.com>
2022-03-02 22:02:34 +0800

    add info

--- template.c
+++ template.c
@@ -1,7 +1,18 @@
+/**
+ * @file template.c
+ * @author CUICUI2010
+ * @brief A C template
+ * @version 0.2
+ * @date 2022-03-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <stdio.h>
 #include <math.h>
 
 int main(){
-    
+
     return 0;
 }
